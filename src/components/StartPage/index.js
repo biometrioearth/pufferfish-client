07e453d52ac2f3efb642472995759a385d639fb1
nerdlet <brndnyokabi@gmail.com>
2023-06-01 12:53:45 +0300

    Initial setup

---
+++ src/components/StartPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StartPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to pufferfish -client
+      </h1>
+    </div>
+  );
+};
+
+export default StartPage;

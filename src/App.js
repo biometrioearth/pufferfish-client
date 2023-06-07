fee15bc0c8a6cf1d265ce47e2370796bc0bb1362
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2023-06-07 13:06:52 +0300

    Dashboard layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import StartPage from "./components/StartPage";
+import Dashboard from "./components/Dashboard";
+import './index.css'
+import Pufferfish from "./components/Pufferfish";
+import Project from "./components/Project";
+import Admin from "./components/Admin";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<StartPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/sites/pufferfish" element={<Pufferfish />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
   );
---
+++ src/components/Project/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const Project = () => {
+  return (
+    <Layout>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+      <p>this is content one: Pufferfish</p>
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <p>this is content two: Pufferfish</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Project;
